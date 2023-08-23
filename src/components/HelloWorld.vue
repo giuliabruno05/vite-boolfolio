471464bf92ac81ee756f2e068a8145bcde9400a4
giuliabruno05 <giuliabruno48@gmail.com>
2023-08-23 13:11:37 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+  export default{
+    name:'helloworld',
+    mounted(){
+      axios.get('http://127.0.0.1:8000/api/v1/project-index')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  }
+
+</script>
+
+<template>
+
+<h2>ciao 2</h2>
+</template>
+
+<style scoped>
+
+</style>
